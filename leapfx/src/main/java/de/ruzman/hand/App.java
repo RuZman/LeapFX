b58578dee015356f0fe0483962c8ea48d43f00ce
RuZman <zoltan@ruzman.de>
2014-10-23 22:02:00 +0200

    Leap Motion integration
    Leap Motion integration

--- leapfx/src/main/java/de/ruzman/hand/App.java
+++ leapfx/src/main/java/de/ruzman/hand/App.java
@@ -1,5 +1,7 @@
 package de.ruzman.hand;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Node;
@@ -10,20 +12,28 @@ import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import de.ruzman.leap.LeapApp;
+import de.ruzman.leap.LeapApp.Mode;
+import de.ruzman.leap.event.PointEvent;
+import de.ruzman.leap.event.PointMotionListener;
 
-public class App extends Application {
+public class App extends Application implements PointMotionListener {	
 
 	public static void main(String[] args) {
+		LeapApp.init(true);
+		LeapApp.setMode(Mode.INTERACTION_BOX);
+		
 		launch(args);
 	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Group group = new Group();
+		Group group = new Group();		
 		Scene scene = new Scene(group, 500, 500);
 		
 		PerspectiveCamera camera = new PerspectiveCamera(true);
-		camera.setTranslateY(10);
+		camera.setTranslateY(100);
 		camera.setRotationAxis(Rotate.X_AXIS);
 		camera.setRotate(90);
 		scene.setCamera(camera);
@@ -32,6 +42,9 @@ public class App extends Application {
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+		synchronizeWithLeapMotion();
+		LeapApp.getMotionRegistry().addPointMotionListener(this);
 	}
 
 	private Node create3DBall() {
@@ -43,5 +56,22 @@ public class App extends Application {
 		sphere.setMaterial(material);
 		
 		return sphere;
+	}
+	
+	private void synchronizeWithLeapMotion() {
+		Timeline timeline = new Timeline();
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		timeline.getKeyFrames().add(
+				new KeyFrame(Duration.seconds(1.0 / 60.0), ea -> LeapApp
+						.update()));
+		timeline.play();
+	}
+	
+	@Override
+	public void pointMoved(PointEvent event) {
+	}
+
+	@Override
+	public void pointDragged(PointEvent event) {
 	}
 }
